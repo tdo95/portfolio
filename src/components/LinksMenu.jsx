606d73b723de6d1971075f45a5c1d4ee40698718
Tee O <tdopress@gmail.com>
2023-02-01 10:48:31 -0500

    edited links menu margin and logo size

--- src/components/LinksMenu.jsx
+++ src/components/LinksMenu.jsx
@@ -4,7 +4,7 @@ const LinksMenu = ({open, isMobile, toggleNav}) => {
   const styles = {
     default: 'flex',
     mobile: 'flex-col self-start gap-6 gap-x-6 bg-primary-purple fixed top-0 right-0 w-64 h-full m-0 p-6 transition-all duration-300 ',
-    regular: 'ml-auto gap-4 gap-x-4 max-[767px]:hidden',
+    regular: 'ml-auto mr-2 gap-4 gap-x-4 max-[767px]:hidden',
     sidebarTransition: `${open ? 'translate-x-0' : 'translate-x-full'}`
   }
   function closeMobileNav() {
