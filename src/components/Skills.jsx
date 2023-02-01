ce79cd675514459d130bb09e11a21dd0428753c9
Tee O <tdopress@gmail.com>
2023-02-01 10:15:14 -0500

    increased skills heading font size and boldness

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,9 +4,9 @@ const Skills = ({skills }) => {
   const styles = {
     default: 'py-5 min-h-[500px]',
     heading: ' max-[375px]:text-4xl flex text-5xl font-bold text-white my-5 after:block after:bg-primary-purple after:w-10 after:h-1 after:flex-1 gap-3 gap-x-3 items-center',
-    skillsContainer: 'flex flex-col gap-10 gap-x-10 py-6',
+    skillsContainer: 'flex flex-col gap-10 gap-x-10 py-6 mb-10',
     skillsCategory: 'flex items-center gap-5 gap-x-5  max-[375px]:flex-col',
-    skillsHeading: 'gap-3 gap-x-3 min-w-[110px] max-w-[110px] text-center font-bold text-gray-200',
+    skillsHeading: 'gap-3 gap-x-3 min-w-[125px] max-w-[125px] text-center text-lg font-extrabold text-gray-200',
     divider: 'block w-px bg-gray-300 self-stretch min-h-[75px] max-[375px]:w-full max-[375px]:min-h-[1px]',
     skillsList: 'flex gap-2 gap-x-2 flex-wrap max-[375px]:justify-center items-center',
     skillItem: 'py-1 px-2  rounded bg-white text-primary-blue  text-center'
